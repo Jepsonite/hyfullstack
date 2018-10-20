36782d27769c5db6ec69a8255b2c3e14b140f32f
Tuomas Manninen <tuomas.manninen@codento.com>
2018-10-20 22:25:47 +0300

    one more after this

---
+++ teht2.14-2.17/src/App.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import AddPerson from './components/AddPerson';
+import FilterShown from './components/FilterShown';
+import personService from './services/persons'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [],
+      newName: '',
+      newNumber: '',
+      filter: ''
+    }
+  }
+
+  componentDidMount() {
+    personService
+      .getAll()
+      .then(response => {
+        this.setState({ persons: response })
+      })
+  }
+
+  addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: this.state.newName,
+      number: this.state.newNumber
+    }
+    if (this.state.persons.map(person => person.name).includes(personObject.name)) {
+      alert("duplikaatti")
+    }
+    else {
+      personService
+        .create(personObject)
+        .then(newPerson => {
+          this.setState({
+            persons: this.state.persons.concat(newPerson),
+            newPerson: '',
+            newNumber: ''
+          })
+        })
+    }
+  }
+
+  handleDeleteOf = (id) => {
+    return () => {
+      const person = this.state.persons.find(p => p.id === id)
+
+      if (window.confirm(`poistetaanko varmasti ${person.name}?`)) {
+        personService
+          .remove(id)
+          .then(response => {
+            this.setState({
+              persons: this.state.persons.filter(p => p.id !== id)
+          })
+        })
+      }
+    }
+  }
+
+  handleNameChange = (event) => {
+    this.setState({ newName: event.target.value })
+  }
+
+  handleNumberChange = (event) => {
+    this.setState({ newNumber: event.target.value })
+  }
+
+  handleFilterChange = (event) => {
+    //console.log(event.target.value)
+    this.setState({ filter: event.target.value })
+  }
+
+  render() {
+    //const shownPersons = () => this.state.persons.map(person => <li>{person.name}</li>)
+    //console.log(this.state.persons.filter(person => person.name.toLowerCase().startsWith(this.state.filter.toLowerCase())))
+    const filteredPersons =
+      this.state.persons.filter(person => person.name.toLowerCase().startsWith(this.state.filter.toLowerCase()))
+
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <div>
+          <FilterShown filter={this.state.filter} handleFilterChange={this.handleFilterChange} />
+        </div>
+        <h3>Lisää uusi</h3>
+        <AddPerson addPerson={this.addPerson} newName={this.state.newName} 
+          handleNameChange={this.handleNameChange} newNumber={this.state.newNumber}
+          handleNumberChange={this.handleNumberChange}/>
+        <h2>Numerot</h2>
+          {filteredPersons.map(person => 
+            <p key={person.name}>
+              {person.name} {person.number} <button onClick={this.handleDeleteOf(person.id)}>poista</button>
+            </p>)}
+      </div>
+    )
+  }
+}
+
+export default App
